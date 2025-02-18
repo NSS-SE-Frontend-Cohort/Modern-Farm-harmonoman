aaab7890f795e94139eb1ff9da096d4c00cdb3fb
Mark Bond <markbond615@gmail.com>
2025-02-18 16:50:26 -0600

    Added unique id's for plants as they're harvested

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -1,15 +1,18 @@
-const harvestedPlants = []
-
 export const harvestPlants = (plants) => {
+    const harvestedPlants = []
+    let idNumber = 1;
+    
     plants.forEach(plant => {
-        // check if plant is corn and if so divide output in half
+        // Check if plant is corn and if so divide output in half
         if (plant.type === "Corn") {
             for (let i = 0; i < plant.output / 2; i++) {
-                harvestedPlants.push(plant);
+                harvestedPlants.push({ ...plant, id: idNumber });
+                idNumber += 1;
             }
         } else {
             for (let i = 0; i < plant.output; i++) {
-                harvestedPlants.push(plant);
+                harvestedPlants.push({ ...plant, id: idNumber });
+                idNumber += 1;
             }
         }
     });
